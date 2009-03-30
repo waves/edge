1efe08987252c02702ea8d3f7f5d418e83486b90
Krishna Krishnamaneni <kkrishnamaneni@attinteractive.com>
2009-03-30 15:31:20 -0700

    Set default to false in the production configuration of compact foundation

--- lib/foundations/compact.rb
+++ lib/foundations/compact.rb
@@ -4,17 +4,17 @@ module Waves
       def self.included( app )
         app.module_eval {
           const_set( :Resources, Module.new {
-            const_set( :Map, Class.new { 
+            const_set( :Map, Class.new {
               include Waves::Resources::Mixin
-              
+
               handler( Exception ) do |e|
                 Waves.debug? ? raise( e ) : Waves::Views::Errors.new( request ).server_error_500
               end
-              
+
               handler( Waves::Dispatchers::NotFoundError ) do |e|
                 Waves.debug? ? raise( e ) : Waves::Views::Errors.new( request ).not_found_404
               end
-              
+
             })
           })
           const_set( :Configurations, Module.new {
@@ -26,6 +26,7 @@ module Waves
               resource app::Resources::Map
             })
             const_set( :Production, Class.new( self::Development ) {
+              debug false
               log :level => :error, :output => ( "log.#{$$}" ), :rotation => :weekly
               port 80
               host '0.0.0.0'
@@ -39,7 +40,7 @@ module Waves
 
                 run ::Waves::Dispatchers::Default.new
               }
-              
+
             })
           })
         }
