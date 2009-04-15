f855484659a26a735dd3aa60fa310139912d103d
Eero Saynatkari <projects@kittensoft.org>
2009-04-15 23:02:55 +0300

    Minor whitespace fix.

--- lib/waves/matchers/uri.rb
+++ lib/waves/matchers/uri.rb
@@ -3,18 +3,18 @@ module Waves
   module Matchers
 
     class URI < Base
-      
+
       def initialize( options )
         @path = Waves::Matchers::Path.new( options[ :path ] )
         @constraints = { :server => options[ :server ], :scheme => options[ :scheme ] }
       end
-    
+
       def call( request )
         @path.call( request ) if test( request )
       end
-      
+
     end
-    
+
   end
-  
+
 end
