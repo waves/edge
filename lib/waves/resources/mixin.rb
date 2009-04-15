596dd86ac48fc51714ec401a21a2b337c8421119
Eero Saynatkari <projects@kittensoft.org>
2009-04-15 22:58:44 +0300

    Temporary debug output for exceptions.

--- lib/waves/resources/mixin.rb
+++ lib/waves/resources/mixin.rb
@@ -81,6 +81,7 @@ module Waves
             rescue Waves::Dispatchers::Redirect => e
               raise e
             rescue Exception => e
+              p :raised, e, e.backtrace, e.message if $DEBUG
               response.status = ( StatusCodes[ e.class ] || 500 )
               ( body = handler( e ) ) rescue raise e
               Waves::Logger.warn "Handled #{e.class}: #{e}"
