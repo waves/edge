b3456cda7baf2542498ff78121083f9bc0dade9c
Eero Saynatkari <projects@kittensoft.org>
2009-04-16 15:35:13 +0300

    Added Ext matcher back, significantly reformatted.

---
+++ lib/waves/matchers/ext.rb
@@ -0,0 +1,50 @@
+module Waves
+  module Matchers
+
+    class Ext < Base
+
+      # Create file extension matcher.
+      #
+      # May be absent, in which case any or no extension is
+      # accepted. Any specified extensions must be given as
+      # Symbols without the dot.
+      #
+      # As a special case, if the extension Array contains
+      # the empty String "", the matcher will match absence
+      # of an extension.
+      #
+      # @todo Enforce MIME lookup success? --rue
+      #
+      def initialize(ext)
+        raise ArgumentError, "No Ext constraints!" unless ext
+
+        ext = Array(ext)
+        raise ArgumentError, "No Ext constraints!" if ext.empty?
+
+        noext = ext.delete ""   # Absent extension
+        ext.map! {|e| ".#{e.to_s}" }
+
+        # @todo Could maybe drop an empty ext. --rue
+        @constraints = {:ext => ext, :noext => noext}
+      end
+
+      # Match URL's file extension.
+      #
+      # @see  Ext#initialize for information.
+      #
+      def call(request)
+        return true if @constraints[:ext].empty?
+
+        if request.ext.empty?
+          return true if @constraints[:noext]
+          return false
+        end
+
+        return @constraints[:ext].any? {|ext| ext == request.ext }
+      end
+
+    end
+
+  end
+
+end
