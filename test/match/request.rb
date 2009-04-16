187f387e2cfe86ac131edc9fc8cb1e2c44c7e578
Eero Saynatkari <projects@kittensoft.org>
2009-04-16 16:18:56 +0300

    Add some more Request matcher stuff.

--- test/match/request.rb
+++ test/match/request.rb
@@ -39,20 +39,52 @@ describe "Top-level request matcher" do
                                      "HTTP_ACCEPT" => "text/html")
     m.call(request).should == {}
   end
+
+  feature "omits Accept constraints if none given" do
+    Waves::Matchers::Request.new({}).constraints.has_key?(:accept).should == false
+  end
+
+  feature "omits Ext constraints if none given" do
+    Waves::Matchers::Request.new({}).constraints.has_key?(:ext).should == false
+  end
+
+  feature "omits Query constraints if none given" do
+    Waves::Matchers::Request.new({}).constraints.has_key?(:query).should == false
+  end
+
+  feature "omits Traits constraints if none given" do
+    Waves::Matchers::Request.new({}).constraints.has_key?(:traits).should == false
+  end
+
+  feature "returns captured values when matched" do
+    m = Waves::Matchers::Request.new  :path => [:something],
+                                      :host => nil,
+                                      :port => nil,
+                                      :scheme => nil
+
+    request = Waves::Request.new env("http://example.com/moo",
+                                     :method => "GET",
+                                     "HTTP_ACCEPT" => "text/html")
+
+    res = m.call request
+    res.kind_of?(Hash).should == true
+    res.size.should == 1
+    res.has_key?(:something).should == true
+  end
+
+  feature "sets the captured trait to return value when matched" do
+    m = Waves::Matchers::Request.new  :path => [:something],
+                                      :host => nil,
+                                      :port => nil,
+                                      :scheme => nil
+
+    request = Waves::Request.new env("http://example.com/moo",
+                                     :method => "GET",
+                                     "HTTP_ACCEPT" => "text/html")
+
+    res = m.call request
+    request.traits.waves.captured.something.should == res[:something]
+  end
+
 end
 
-
-
-
-
-#      def initialize(options)
-#        # Simplest to fake it if there is no URL to match
-#        @uri = Matchers::URI.new(options) rescue lambda { {} }
-#
-#        @constraints = {}
-#
-#        # These are essentially optional
-#        maybe { @constraints[:accept] = Matchers::Accept.new options }
-#        maybe { @constraints[:ext]    = Matchers::Ext.new options[:ext] }
-#        maybe { @constraints[:query]  = Matchers::Query.new options[:query] }
-#        maybe { @constraints[:traits] = Matchers::Traits.new options[:traits] }
