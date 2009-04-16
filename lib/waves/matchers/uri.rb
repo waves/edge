5452fd5ba8770e316cce24c070f3f7376bdacc5f
Eero Saynatkari <projects@kittensoft.org>
2009-04-16 15:39:30 +0300

    Omit nil path matchers from URI matcher.

--- lib/waves/matchers/uri.rb
+++ lib/waves/matchers/uri.rb
@@ -3,6 +3,10 @@ module Waves
   module Matchers
 
     class URI < Base
+      def initialize(options)
+        # Simplest to fake it if there is no path to match
+        @path = Waves::Matchers::Path.new(options[:path]) rescue lambda { {} }
+        @constraints = {}
 
       def initialize( options )
         @path = Waves::Matchers::Path.new( options[ :path ] )
