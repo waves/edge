8a5eff66581e0166d7a444f90c9d7f6c3dd48efe
Eero Saynatkari <projects@kittensoft.org>
2009-04-22 17:50:52 +0300

    Minor spec description change.

--- spec/foundations/rest/resource_definition_spec.rb
+++ spec/foundations/rest/resource_definition_spec.rb
@@ -12,11 +12,11 @@ describe "A resource definition" do
     Object.send :remove_const, :DefSpec if Object.const_defined?(:DefSpec)
   end
 
-  it "takes a single Symbol argument" do
+  it "takes a single Symbol argument for the resource name" do
     lambda { resource(:DefSpec) {} }.should_not raise_error
   end
 
-  it "defines a class using given argument as its name" do
+  it "defines a class using the given name" do
     Object.const_defined?(:DefSpec).should == false
 
     resource(:DefSpec) {}
