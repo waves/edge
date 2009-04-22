3215a52168ca4d30cd7c6d65077e3984d1bb1bf8
Eero Saynatkari <projects@kittensoft.org>
2009-04-22 21:59:02 +0300

    Actually start logic in REST Foundation viewability specs.

--- spec/foundations/rest/viewability_spec.rb
+++ spec/foundations/rest/viewability_spec.rb
@@ -2,6 +2,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_hel
 
 # Our stuff
 require "waves/foundations/rest"
+require "waves/matchers/request"
 
 describe "Viewability definition for a resource" do
   before :all do
@@ -19,4 +20,66 @@ describe "Viewability definition for a resource" do
       end
     }.should_not raise_error
   end
+end
+
+describe "The set of representations supported by a resource" do
+  before :all do
+    Object.send :remove_const, :ViewSpec if Object.const_defined?(:ViewSpec)
+  end
+
+  after :each do
+    Object.send :remove_const, :ViewSpec if Object.const_defined?(:ViewSpec)
+  end
+
+  it "is defined through the #representation method inside a #viewable block" do
+    lambda {
+      resource :ViewSpec do
+        viewable {
+          representation {}
+        }
+      end
+    }.should_not raise_error
+  end
+end
+
+describe "A representation definition" do
+  before :all do
+    Object.send :remove_const, :ViewSpec if Object.const_defined?(:ViewSpec)
+  end
+
+  after :each do
+    Object.send :remove_const, :ViewSpec if Object.const_defined?(:ViewSpec)
+  end
+
+  it "takes the MIME type the representation is for" do
+    lambda {
+      resource :ViewSpec do
+        viewable {
+          representation("text/html") {}
+        }
+      end
+    }.should_not raise_error
+  end
+
+  it "allows more than one MIME type to be specified" do
+    lambda {
+      resource :ViewSpec do
+        viewable {
+          representation("text/html", "application/xml+xhtml") {}
+        }
+      end
+    }.should_not raise_error
+  end
+
+  it "defines a matcher for the specified type" do
+    # @todo This is somewhat unscientific. --rue
+    mock(Waves::Matchers::Request).new(hash_including(:accept => %w[text/javascript]))
+
+    resource :ViewSpec do
+      viewable {
+        representation("text/javascript") {}
+      }
+    end
+  end
+
 end
