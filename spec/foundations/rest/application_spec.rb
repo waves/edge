1ca576394070d0f5b5eedd90c7aa887e0b955ca4
Eero Saynatkari <projects@kittensoft.org>
2009-04-24 16:17:39 +0300

    Start Application specs.

--- spec/foundations/rest/application_spec.rb
+++ spec/foundations/rest/application_spec.rb
@@ -6,15 +6,28 @@ require "waves/foundations/rest"
 include Waves::Foundations
 
 describe "The Application" do
-  before :all do
-    Object.send :remove_const, :DefSpec if Object.const_defined?(:DefSpec)
-  end
 
   after :each do
-    Object.send :remove_const, :DefSpec if Object.const_defined?(:DefSpec)
+    REST::Application.send :remove_const, :MyApp if REST::Application.const_defined?(:MyApp)
   end
 
   # @todo Much fleshing out here. Overrides and such. --rue
+
+  it "requires a name" do
+    lambda {
+      application {}
+    }.should raise_error
+
+    lambda {
+      application(:MyApp) {}
+    }.should_not raise_error
+  end
+
+  it "is created as named constant under REST::Application" do
+    REST::Application.const_defined?(:MyApp).should == false
+    application(:MyApp) {}
+    REST::Application.const_defined?(:MyApp).should == true
+  end
 
   it "gives some full URL form when supplied a resource and its specifics" do
     res = Class.new(REST::Resource) {}
@@ -23,4 +36,22 @@ describe "The Application" do
     ret.should include(:whatever)
   end
 
+end
+
+describe "Currently active Application" do
+  before :all do
+    Object.send :remove_const, :DefSpec if Object.const_defined?(:DefSpec)
+  end
+
+  after :each do
+    Object.send :remove_const, :DefSpec if Object.const_defined?(:DefSpec)
+  end
+
+  it "is not defined outside of an application definition block" do
+    fail
+  end
+
+  it "is the application whose definition we are in" do
+    fail
+  end
 end
