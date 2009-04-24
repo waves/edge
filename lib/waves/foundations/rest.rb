740153194853378817b153df3d329474bd56e304
Eero Saynatkari <projects@kittensoft.org>
2009-04-24 20:23:52 +0300

    Provide .needs_from_url for resources.

--- lib/waves/foundations/rest.rb
+++ lib/waves/foundations/rest.rb
@@ -95,6 +95,14 @@ module Waves
           @method = nil
         end
 
+        # Pathspec this resource needs for processing.
+        #
+        # @see  .url_of_form()
+        #
+        def self.needs_from_url()
+          @pathspec
+        end
+
         # Representation definition block
         #
         def self.representation(*types, &block)
