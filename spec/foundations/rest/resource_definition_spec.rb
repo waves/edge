99394f25655771e5113168ac7fe89e2ac9287897
Eero Saynatkari <projects@kittensoft.org>
2009-04-30 22:28:37 +0300

    Spec introducing new MIME types in resources.

--- spec/foundations/rest/resource_definition_spec.rb
+++ spec/foundations/rest/resource_definition_spec.rb
@@ -45,7 +45,6 @@ describe "A resource definition" do
     ResDefModule::ResourceDefSpec.class.should == Class
   end
 
-
   it "defines a class with given name as constant if not nested" do
     Object.const_defined?(:ResourceDefSpec).should == false
 
@@ -135,5 +134,123 @@ describe "A resource definition" do
 
     ResourceDefSpec.needed_from_url.should == pathspec
   end
+
+  it "allows introducing new MIME types and their extensions" do
+    mock(Kernel).load(anything) {
+      resource :ResourceDefSpec do
+        url_of_form [:hi]
+
+        introduce_mime "vnd.hi.ho", :exts => ".hiho"
+      end
+    }
+
+    Waves::MimeTypes[".hiho"].should == []
+
+    stub.instance_of(Waves.main::Mounts).to { true }
+
+    request = Waves::Request.new env("http://example.com/defspec", :method => "GET")
+    Waves.main::Mounts.new(request).process
+
+    Waves::MimeTypes[".hiho"].should == ["vnd.hi.ho"]
+  end
+
+  it "raises an error if no extensions are given for an introduced MIME type" do
+    mock(Kernel).load(anything) {
+      lambda {
+        resource :ResourceDefSpec do
+          url_of_form [:hi]
+          introduce_mime "vnd.ho.hi"
+        end
+      }.should raise_error(ArgumentError)
+      true
+    }
+
+    stub.instance_of(Waves.main::Mounts).to { true }
+
+    request = Waves::Request.new env("http://example.com/defspec", :method => "GET")
+    Waves.main::Mounts.new(request).process
+  end
+
+  it "allows a MIME type to specify multiple extensions" do
+    mock(Kernel).load(anything) {
+      resource :ResourceDefSpec do
+        url_of_form [:hi]
+
+        introduce_mime "vnd.moo.mii", :exts => [".moomii", ".miimoo"]
+      end
+    }
+
+    Waves::MimeTypes[".moomii"].should == []
+
+    stub.instance_of(Waves.main::Mounts).to { true }
+
+    request = Waves::Request.new env("http://example.com/defspec", :method => "GET")
+    Waves.main::Mounts.new(request).process
+
+    Waves::MimeTypes[".moomii"].should == ["vnd.moo.mii"]
+    Waves::MimeTypes[".miimoo"].should == ["vnd.moo.mii"]
+  end
+
+  it "allows adding a MIME type multiple times" do
+    mock(Kernel).load(anything) {
+      resource :ResourceDefSpec do
+        url_of_form [:hi]
+
+        introduce_mime "vnd.a.b", :exts => ".aabi"
+        introduce_mime "vnd.a.b", :exts => ".uubi"
+      end
+    }
+
+    Waves::MimeTypes[".aabi"].should == []
+
+    stub.instance_of(Waves.main::Mounts).to { true }
+
+    request = Waves::Request.new env("http://example.com/defspec", :method => "GET")
+    Waves.main::Mounts.new(request).process
+
+    Waves::MimeTypes[".aabi"].should == ["vnd.a.b"]
+    Waves::MimeTypes[".uubi"].should == ["vnd.a.b"]
+  end
+
+  it "ensures that the mappings only have a single instance of each extension" do
+    mock(Kernel).load(anything) {
+      resource :ResourceDefSpec do
+        url_of_form [:hi]
+
+        introduce_mime "vnd.b.a", :exts => ".beebi"
+        introduce_mime "vnd.b.a", :exts => ".beebi"
+      end
+    }
+
+    Waves::MimeTypes[".beebi"].should == []
+
+    stub.instance_of(Waves.main::Mounts).to { true }
+
+    request = Waves::Request.new env("http://example.com/defspec", :method => "GET")
+    Waves.main::Mounts.new(request).process
+
+    Waves::MimeTypes[".beebi"].should == ["vnd.b.a"]
+  end
+
+  it "adds the associated MimeExts inverse lookup when introducing MIME types" do
+    mock(Kernel).load(anything) {
+      resource :ResourceDefSpec do
+        url_of_form [:hi]
+
+        introduce_mime "vnd.weeblie", :exts => [".weeblex", ".beebliew"]
+      end
+    }
+
+    Waves::MimeExts["vnd.weeblie"].should == []
+
+    stub.instance_of(Waves.main::Mounts).to { true }
+
+    request = Waves::Request.new env("http://example.com/defspec", :method => "GET")
+    Waves.main::Mounts.new(request).process
+
+    Waves::MimeExts["vnd.weeblie"].include?(".beebliew").should == true
+    Waves::MimeExts["vnd.weeblie"].include?(".weeblex").should == true
+  end
+
 end
 
