a7492c1507c89254fb1319e97c132484249e9123
Krishna Krishnamaneni <kkrishnamaneni@attinteractive.com>
2009-07-10 12:50:39 -0700

    Added database initialization back in

--- lib/waves/layers/orm/providers/sequel.rb
+++ lib/waves/layers/orm/providers/sequel.rb
@@ -27,6 +27,8 @@ module Waves
           require "#{File.dirname(__FILE__)}/sequel/tasks/generate" if defined?(Rake)
 
           def app.database ; @sequel ||= ::Sequel.connect( Waves.config.database ) ; end
+	  #Make a connection
+	  app.database
           app.auto_create_module( :Models ) do
             auto_create_class :Default, ::Sequel::Model
             auto_load :Default, :directories => [ :models ]
